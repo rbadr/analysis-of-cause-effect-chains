b86b671c3e8537feaecebb0b93ede19265b516e0
rbadr <rahal.badr@gmail.com>
2016-12-21 21:47:47 +0100

    unit test

---
+++ src/test/java/fr/univlille1/m2iagl/opl/causechaineffect/action/helper/HelperTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.univlille1.m2iagl.opl.causechaineffect.action.helper;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Rahal Badr
+ */
+public class HelperTest {
+    
+    public HelperTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getKeysWithDifferentValues method, of class Helper.
+     */
+    @Test
+    public void testGetKeysWithDifferentValues() {
+        System.out.println("getKeysWithDifferentValues");
+        Map<String, Object> firstMap = null;
+        Map<String, Object> secondMap = null;
+        List<String> expResult = null;
+        List<String> result = Helper.getKeysWithDifferentValues(firstMap, secondMap);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
